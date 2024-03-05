2fd11f757875ca7436f28f261081e031a6aaaef9
Zxcalex <Zxcalex@DESKTOP-P5N7BAU>
2024-03-06 01:03:52 +0500

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+class ToDo
+{
+    private string Name;
+    private string DateDo;
+    private string Discr;
+    public ToDo(string name, string dateDo, string discr)
+    {
+        Name = name;
+        DateDo = dateDo;
+        Discr = discr;
+    }
+    public string NameS
+    {
+        get { return Name; }
+        set { Name = value; }
+    }
+    public string DateDoS
+    {
+        get { return DateDo; }
+        set { DateDo = value; }
+    }
+    public string DiscrS
+    {
+        get { return Discr; }
+        set { Discr = value; }
+    }
+
+}
+
+namespace lab13
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<ToDo> strList = new List<ToDo>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            dateToDo.SelectedDate = new DateTime(2024, 01, 10);
+            descriptionToDo.Text = "Описания нет";
+        }
+
+        private void button_Add_Click(object sender, RoutedEventArgs e)
+        {            
+            if (dateToDo.SelectedDate == null){MessageBox.Show("No date selected"); return;}
+            mainListBox.ItemsSource = null;
+            strList.Add(new ToDo(titleToDo.Text, dateToDo.SelectedDate.Value.ToShortDateString(), descriptionToDo.Text));
+            mainListBox.ItemsSource = strList;
+            titleToDo.Text = null;
+            descriptionToDo.Text = null;
+            dateToDo.SelectedDate = null;
+        }
+        private void Button_delete_Click(object sender, RoutedEventArgs e)
+        {
+            strList.Remove(strList[mainListBox.SelectedIndex]);
+            mainListBox.ItemsSource = null;
+            mainListBox.ItemsSource = strList;
+        }
+    }
+}
